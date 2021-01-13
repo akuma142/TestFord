ee5671694c651aca71b95bcd9d255959e6419fd4
Attiq <ashu.pat793@gmail.com>
2021-01-13 07:26:26 -0800

    Commit second

--- DockerDemo/src/main/java/com/example/demo/DockerDemoApplication.java
+++ DockerDemo/src/main/java/com/example/demo/DockerDemoApplication.java
@@ -2,9 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
+	@GetMapping("/a")
+	public String  message()
+	{
+		return "Hellow to Docker World!!";
+	}
+	@GetMapping("/b")
+	public String  message2(String message)
+	{
+		return message+"Hellow to Docker World!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
